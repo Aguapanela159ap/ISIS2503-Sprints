25621b985c55cc4a17db25c66880871dc7532a3d
LexBetancourt <l.betancourte@uniandes.edu.co>
2024-10-18 17:47:17 -0500

    correccion_urlspy

--- cronogramas/urls.py
+++ cronogramas/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.cronograma_list, name='cronograma_list'),  # Lista de cronogramas
     path('<int:pk>/', views.cronograma_detail, name='cronograma_detail'),  # Detalle del cronograma
     path('nuevo/', views.cronograma_create, name='cronograma_create'),  # Crear un cronograma
+    path('delete/<int:cronograma_id>/', views.cronograma_delete, name='cronograma_delete'),
 ]
